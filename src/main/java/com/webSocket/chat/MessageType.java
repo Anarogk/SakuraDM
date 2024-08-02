43c0af28c6965f99d9452b63da9e518ea9194188
Anurag <anuragmunde002@gmail.com>
2024-08-03 02:49:13 +0530

    initial commit with readme and main firsts.

---
+++ src/main/java/com/webSocket/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.webSocket.chat;
+
+// enum class for availablility status
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ src/test/java/com/websocchat/websock/WebsockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocchat.websock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
